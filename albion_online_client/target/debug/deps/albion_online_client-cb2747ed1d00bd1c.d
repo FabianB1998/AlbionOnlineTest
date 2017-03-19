2cbe70793b49db9c10b849d6a9df6ec227267056
fbrand <fabian@fabianbrand.de>
2017-03-19 15:07:20 +0100

    Failed artifacts

--- albion_online_client/target/debug/deps/albion_online_client-cb2747ed1d00bd1c.d
+++
@@ -1,6 +0,0 @@
-/home/fbrand/AlbionOnlineTest/albion_online_client/target/debug/deps/albion_online_client-cb2747ed1d00bd1c: main.rs user.rs
-
-/home/fbrand/AlbionOnlineTest/albion_online_client/target/debug/deps/albion_online_client-cb2747ed1d00bd1c.d: main.rs user.rs
-
-main.rs:
-user.rs:
